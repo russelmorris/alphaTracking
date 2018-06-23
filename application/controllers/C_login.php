ac3d48170c00aef4b413f89202768666af8c1a35
NikolaMalezanoski <malezan@gmail.com>
2018-06-23 10:08:44 +0800

    Setup codeigniter and login page

---
+++ application/controllers/C_login.php
@@ -0,0 +1,34 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class C_login extends CI_Controller {
+
+	/**
+	 * Index Page for this controller.
+	 *
+	 * Maps to the following URL
+	 * 		http://example.com/index.php/welcome
+	 *	- or -
+	 * 		http://example.com/index.php/welcome/index
+	 *	- or -
+	 * Since this controller is set as the default controller in
+	 * config/routes.php, it's displayed at http://example.com/
+	 *
+	 * So any other public methods not prefixed with an underscore will
+	 * map to /index.php/welcome/<method_name>
+	 * @see https://codeigniter.com/user_guide/general/urls.html
+	 */
+	public function login()
+	{
+	    // check if request is POST
+        if ($this->input->server('REQUEST_METHOD') == 'POST'){
+            redirect('/dashboard');
+        }
+        $data = [];
+        $data['csrf'] = array(
+            'name' => $this->security->get_csrf_token_name(),
+            'hash' => $this->security->get_csrf_hash()
+        );
+		$this->load->view('v_login', $data);
+	}
+}
