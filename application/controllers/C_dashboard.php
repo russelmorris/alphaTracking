51fb6f557f03436f283c4ffc45d425ad28709ac1
Emil Bah <bahtijaroski_emil@yahoo.com>
2018-07-13 19:48:35 +0200

    added function for closest date

--- application/controllers/C_dashboard.php
+++ application/controllers/C_dashboard.php
@@ -186,7 +186,7 @@ class C_dashboard extends MY_Controller
             $datetime1 = new DateTime($day);
             $datetime2 = new DateTime($date);
             $interval = $datetime1->diff($datetime2);
-            if($interval === 0){
+            if($interval === 1){
                 $closest = $day;
             }
             break;
