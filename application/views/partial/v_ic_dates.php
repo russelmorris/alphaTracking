c58350c1a99c38e09ef6b3cc12174b494e3048ef
malezan <malezan@gmail.com>
2018-09-24 15:48:43 +0800

    current is date

--- application/views/partial/v_ic_dates.php
+++ application/views/partial/v_ic_dates.php
@@ -3,10 +3,24 @@
         <div class="col-md-12">
             <h1>IC Dates</h1>
         </div>
-        <div class="col-md-12 no-padding">
-            <a class="btn btn-default pull-right mb-5" href="<?php echo base_url('add-ic-date')?>"> Add new Date </a>
+    </div>
+    <div class="row">
+        <div class="col col-md-6">
+            <div class="form-group form-inline no-padding">
+                <label for="current-ic-date"><h4>Current ICDate:</h4> </label>
+                <select class="form-control" id="current-ic-date">
+                    <?php foreach ($ic_dates as $ic_date) { ?>
+                        <option><?php echo $ic_date['icDate'] ?></option>
+                    <?php } ?>
+                </select>
+                <button class="btn btn-primary" type="button"> Set Curenet date</button>
+            </div>
         </div>
-
+        <div class="col-md-6 no-padding pull-right">
+            <a class="btn btn-primary pull-right mb-5" href="<?php echo base_url('add-ic-date') ?>"> Add new Date </a>
+        </div>
+    </div>
+    <div class="row">
         <table width="100%" class="table table-striped table-bordered table-hover">
             <thead>
             <tr>
@@ -20,17 +34,17 @@
             </tr>
             </thead>
             <tbody>
-                <?php foreach ($ic_dates as $ic_date) { ?>
-                    <tr>
-                        <td><?php echo $ic_date['icDate']; ?></td>
-                        <td><?php echo $ic_date['strategyNo']; ?></td>
-                        <td><?php echo $ic_date['planExecDate']; ?></td>
-                        <td><?php echo $ic_date['planNextExecDate']; ?></td>
-                        <td><?php echo $ic_date['portfolioCount']; ?></td>
-                        <td><?php echo $ic_date['machineRunDate']; ?></td>
-                        <td><?php echo $ic_date['nextMachineRunDate']; ?></td>
-                    </tr>
-                <?php }; ?>
+            <?php foreach ($ic_dates as $ic_date) { ?>
+                <tr>
+                    <td><?php echo $ic_date['icDate']; ?></td>
+                    <td><?php echo $ic_date['strategyNo']; ?></td>
+                    <td><?php echo $ic_date['planExecDate']; ?></td>
+                    <td><?php echo $ic_date['planNextExecDate']; ?></td>
+                    <td><?php echo $ic_date['portfolioCount']; ?></td>
+                    <td><?php echo $ic_date['machineRunDate']; ?></td>
+                    <td><?php echo $ic_date['nextMachineRunDate']; ?></td>
+                </tr>
+            <?php }; ?>
             </tbody>
         </table>
 
